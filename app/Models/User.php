44845e1b5db02b99113e7ef1258a5567c2509d70
jw <15910284120@163.com>
2018-07-16 12:07:50 +0000

    Move user model to models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,12 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    protected $table = 'users';
+    protected $fillable = ['name','email','password'];
+    protected $hidden = ['password','remember_token'];
     use Notifiable;
 
     /**
